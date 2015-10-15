17999db16b31f4a7037ea5cbff541555623346c9
Boernsi <bernhard@linux-stammtisch.at>
2015-10-15 22:00:16 +0200

    Neues Python Script NHW Countdown

---
+++ nhw_countdown.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+# Author: Bernhard Humer
+# Ziel: zählt die Tage bis zum Auszug der Fam. NHW
+
+from datetime import date
+
+heute = date.today()
+auszug = date(2016, 1, 1)
+
+countdown = auszug - heute
+
+print(countdown, ' Tage')
+
